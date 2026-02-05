46a39cf5ede7055f267f3e2e52d76c52ece9c6b0
vinhkhanh <khanhnguyeen2208@gmail.com>
2026-02-06 00:03:19 +0700

    update

--- ASM2/src/main.js
+++ ASM2/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+//import css and js for bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+//import custom css
+import './style.css'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
