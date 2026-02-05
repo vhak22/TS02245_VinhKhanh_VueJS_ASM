c78a7170fbb0e128e21e0308eab094b90d5a66af
vinhkhanh <khanhnguyeen2208@gmail.com>
2026-02-06 00:06:35 +0700

    router setup index.js

---
+++ ASM2/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import PostDetailView from '../views/PostDetailView.vue'
+import AdminView from '../views/AdminView.vue'
+import ProfileView from '../views/ProfileView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/', name: 'home', component: HomeView },
+        { path: '/login', name: 'login', component: LoginView },
+        { path: '/register', name: 'register', component: RegisterView },
+        { path: '/post/:id', name: 'post-detail', component: PostDetailView },
+        { path: '/profile', name: 'profile', component: ProfileView },
+        { path: '/admin', name: 'admin', component: AdminView }
+    ]
+})
+
+
+
+export default router
